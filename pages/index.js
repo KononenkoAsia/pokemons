b8141554d0e59c507317990b5fd57cfea76d0596
KononenkoAsya <ananaskononenko@gmail.com>
2019-04-02 12:54:21 +0300

    Create welcome page and Server Side Support

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+class Index extends Component {
+  render() {
+    return <div>HELLO </div>
+  }
+}
+
+export default Index
